4075b20c562e3e1f3b94721bee81107a4587d5eb
Aneesh <aneeshappid@gmail.com>
2025-03-14 23:31:08 +0530

    Completed Hero Section

---
+++ components/sub/HeroContent.tsx
@@ -0,0 +1,68 @@
+'use client'
+
+import React from 'react'
+import {motion} from 'framer-motion'
+import { slideInFromLeft, slideInFromRight, slideInFromTop } from '@/utils/motion'
+import { SparklesIcon } from '@heroicons/react/24/solid'
+import Image from 'next/image'
+
+const HeroContent = () => {
+  return (
+    <motion.div
+    initial="hidden"
+    animate="visible"
+    className='flex items-center justify-center
+     px-20 mt-40 w-full z-[20]'
+     >
+      <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
+        <motion.div
+        className='Welcome-box py-[8px] px-[4px] border border-[#7042f88b] opacity-[0.9]'
+        variants={slideInFromTop}
+        >
+          <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
+          <h1 className='Welcome-text text-[13px]'>FullStack Developer Portfolio</h1>
+        </motion.div>
+
+        <motion.p
+        variants={slideInFromLeft(0.5)}
+        className='flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] h-auto w-auto'
+        >
+          <span>
+          Providing 
+          <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'> the best </span>
+          project experience
+          </span>
+        </motion.p>
+
+        <motion.p
+        variants={slideInFromLeft(0.8)}
+        className='text-lg text-gray-400 my-5 max-w-[600px]'
+        >
+          I&apos;m a Full Stack Developer with experience in Website and Software Development. Check out my projects and skills.
+        </motion.p>
+
+        <motion.a
+        variants={slideInFromLeft(1)}
+        className='py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]'
+        >
+          Learn More.
+        </motion.a>
+      </div>
+
+      <motion.div
+      variants={slideInFromRight(0.8)}
+      className='w-full h-full flex justify-center items-center'
+      >
+        <Image
+        src="/mainIconsdark.svg"
+        alt='work icons'
+        height={650}
+        width={650}
+        />
+      </motion.div>
+      
+    </motion.div>
+  )
+}
+
+export default HeroContent
